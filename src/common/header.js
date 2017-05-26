15739fa9d29c0f3f0b260f4869f355a7e785e17c
Yoshiya Hinosawa <stibium121@gmail.com>
2017-05-26 14:18:45 +0900

    feat(app-menu): polish app-menu behavior

--- src/common/header.js
+++ src/common/header.js
@@ -2,16 +2,45 @@ const { component, on } = capsid
 
 @component('app-menu-btn')
 class AppMenu {
-  @on('click') open () {
-    const $target = $('.app-menu-dropdown').css('display', 'block')
+  __init__ () {
+    $('body').on('click', e => {
+      if ($(e.target).closest('.app-menu-dropdown').length === 0) {
+        this.close()
+      }
+    })
 
-    setTimeout(() => {
-      $target.toggleClass('is-visible')
+    this.transitioning = false
+
+    this.$target = $('.app-menu-dropdown')
       .on('transitionend', e => {
-        if (!$target.hasClass('is-visible')) {
-          $target.css('display', 'none')
+        if (e.originalEvent.propertyName !== 'opacity') {
+          return
+        }
+
+        this.transitioning = false
+
+        if (!this.$target.hasClass('is-visible')) {
+          this.$target.css('display', 'none')
         }
       })
-    })
+  }
+
+  @on('click') open (e) {
+    if (this.transitioning) { return }
+    if (this.$target.hasClass('is-visible')) { return }
+    this.transitioning = true
+
+    e.stopPropagation()
+
+    this.$target.css('display', 'block')
+    requestAnimationFrame(() => this.$target.addClass('is-visible'))
+  }
+
+  close () {
+    if (this.transitioning) { return }
+    if (!this.$target.hasClass('is-visible')) { return }
+    this.transitioning = true
+
+    this.$target.removeClass('is-visible')
   }
 }
