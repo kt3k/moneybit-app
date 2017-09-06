e134ca3ad4cac8a5c797da4da32b86cd4f9fafd8
Yoshiya Hinosawa <stibium121@gmail.com>
2017-09-06 23:57:19 +0900

    chore: remove unnecessary tests

--- src/app/__tests__/model-hub.js
+++ src/app/__tests__/model-hub.js
@@ -5,19 +5,6 @@ const { expect } = require('chai')
 const td = require('testdouble')
 
 describe('ModelHub', () => {
-  describe('__init__', () => {
-    it('initializes the user', async () => {
-      const modelHub = new ModelHub()
-      const user = new User({ id: 'foo' })
-
-      td.replace(modelHub.initService, 'init')
-      td.when(modelHub.initService.init()).thenResolve(user)
-
-      modelHub.el = document.createElement('div')
-
-      await modelHub.__init__()
-
-      expect(modelHub.user).to.equal(user)
-    })
-  })
+  describe('onModelSave', () => {})
+  describe('save', () => {})
 })
