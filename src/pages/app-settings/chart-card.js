987714007d89f7a7cec91959c4f192b6c8fb70a2
Yoshiya Hinosawa <stibium121@gmail.com>
2017-10-01 23:35:08 +0900

    feat: add chart-card to app-settings

---
+++ src/pages/app-settings/chart-card.js
@@ -0,0 +1,25 @@
+const { MODEL_UPDATE } = require('../../app/action-types')
+const { component, wire, on } = capsid
+const genel = require('genel')
+
+@component
+class ChartCard {
+  @wire.el('tbody') get tbody () {}
+
+  @on(MODEL_UPDATE)
+  onModelUpdate ({ detail: { defaultChart, domain: { MajorAccountType } } }) {
+    const majorType = MajorAccountType[this.el.getAttribute('type')]
+
+    if (!majorType) {
+      throw new Error(`no such type: "${this.el.getAttribute('type')}"`)
+    }
+
+    this.tbody.innerHTML = ''
+
+    defaultChart.getAccountTypesByMajorType(majorType).forEach(type => {
+      this.tbody.appendChild(genel.tr`<td><a href="#">${type.name}</a></td>`)
+    })
+  }
+}
+
+module.exports = ChartCard
