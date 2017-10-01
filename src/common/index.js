b4176f2579362d9931bb19c90c3ed6f43660f8c2
Yoshiya Hinosawa <stibium121@gmail.com>
2017-10-01 16:22:55 +0900

    chore: add event logging

--- src/common/index.js
+++ src/common/index.js
@@ -1,5 +1,8 @@
 require('babel-polyfill')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('capsid/debug')
+}
 global.basepath = process.env.BASEPATH || ''
 global.t10 = require('t10')
 global.capsid = require('capsid')
