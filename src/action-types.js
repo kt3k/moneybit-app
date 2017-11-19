2f095337e1cb6b20554f4b65c04487592802c571
Yoshiya Hinosawa <stibium121@gmail.com>
2017-11-19 17:26:03 +0900

    chore: move action-types

---
+++ src/action-types.js
@@ -0,0 +1,29 @@
+// hub
+exports.HUB_READY = 'mb/hub/READY'
+// ui
+exports.UI_READY = 'mb/ui/READY'
+// app state
+exports.INIT_APP_STATE = 'mb/app-state/INIT'
+exports.APP_STATE_READY = 'mb/app-state/READY'
+// location
+exports.CHECK_LOCATION = 'mb/location/CHECK'
+exports.LOCATION_OK = 'mb/location/OK'
+exports.LOCATION_NG = 'mb/location/NG'
+// user
+exports.INIT_USER = 'mb/user/INIT'
+exports.USER_READY = 'mb/user/READY'
+// journal document
+exports.CREATE_JOURNAL_DOCUMENT = 'mb/journal-document/CREATE'
+exports.CHANGE_CURRENT_DOCUMENT = 'mb/journal-document/CHANGE'
+exports.UPDATE_CURRENT_DOCUMENT = 'mb/journal-document/UPDATE'
+// language
+exports.SWITCH_LANGUAGE = 'mb/language/SWITCH'
+exports.INIT_LANGUAGE = 'mb/language/INIT'
+exports.UI_LANGUAGE_READY = 'mb/language/READY'
+// chart
+exports.INIT_CHART = 'mb/chart/LOAD'
+exports.CHART_READY = 'mb/chart/READY'
+// models
+exports.MODEL_SAVE = 'mb/model/SAVE'
+exports.MODEL_SAVE_AND_RELOAD = 'mb/model/SAVE_AND_RELOAD'
+exports.MODEL_UPDATE = 'mb/model/UPDATE'
