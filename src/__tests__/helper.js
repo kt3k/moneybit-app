6f26e577382e63a903eada3d251a87692a21100e
Yoshiya Hinosawa <stibium121@gmail.com>
2017-11-29 12:55:57 +0900

    test: add test of ModelHub#notifyUpdate

--- src/__tests__/helper.js
+++ src/__tests__/helper.js
@@ -4,3 +4,4 @@ global.$ = require('jquery')
 global.t10 = require('t10')
 global.basepath = '/'
 global.capsid = require('capsid')
+global.domain = require('../domain')
--- src/model-hub/__tests__/index.js
+++ src/model-hub/__tests__/index.js
@@ -1,6 +1,30 @@
-const { describe } = require('kocha')
+const { describe, it } = require('kocha')
+const ModelHub = require('../')
+const genel = require('genel')
+const { expect } = require('chai')
+const { Action } = require('~')
+
+const { make } = capsid
 
 describe('ModelHub', () => {
-  describe('onModelSave', () => {})
-  describe('save', () => {})
+  describe('onModelSave', () => {
+  })
+
+  describe('save', () => {
+  })
+
+  describe('notifyUpdate', () => {
+    it('notifies the model update', async () => {
+      const hub = make('js-model-hub', genel.div`<p class="is-model-observer">hello</p>`)
+      hub.languageReady = Promise.resolve()
+
+      const p = new Promise(resolve => {
+        hub.el.firstChild.addEventListener(Action.MODEL_UPDATE, () => resolve())
+      })
+
+      expect(await hub.notifyUpdate()).to.equal(hub)
+
+      return p
+    })
+  })
 })
