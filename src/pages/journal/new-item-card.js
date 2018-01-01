d54e605efc8ccba00d079be53e31aedefd7aea23
Yoshiya Hinosawa <stibium121@gmail.com>
2018-01-01 20:11:15 +0900

    feat: collect new-item-card parameters

--- src/pages/journal/new-item-card.js
+++ src/pages/journal/new-item-card.js
@@ -1,28 +1,48 @@
-const {
-  capsid: {
-    component,
-    on,
-    emits
-  },
-  actions: {
-    CREATE_TRADE
-  }
-} = require('~')
+const { capsid: { component, on, emits, wired }, Action } = require('~')
 
 @component('js-new-item-card')
 export default class NewItemCard {
+  @wired('.new-item-card__date') get date () {}
+  @wired('.new-item-card__desc') get desc () {}
+  @wired.all('.new-item-card__debit') get debits () {}
+  @wired.all('.new-item-card__credit') get credits () {}
+
   @on('click', { at: '.new-item-save-button' })
-  @emits(CREATE_TRADE)
+  @emits(Action.CREATE_TRADE)
   onCreateNewTrade () {
-    return {
-      date: '2017-12-15',
-      desc: '売上12月分',
-      dr: {
-        '売上': 100000
-      },
-      cr: {
-        '現金': 100000
-      }
-    }
+    const date = this.date.dataset.date
+    const desc = this.desc.value
+    const dr = this.createDebitObject()
+    const cr = this.createCreditObject()
+
+    return { date, desc, dr, cr }
+  }
+
+  validate () {
+    console.log('TODO: validate')
+  }
+
+  createDebitObject () {
+    const dr = {}
+
+    ;[].forEach.call(this.debits, row => {
+      const type = row.querySelector('.new-item-card__debit-type').value
+      const amount = +row.querySelector('.new-item-card__debit-amount').value
+      dr[type] = amount
+    })
+
+    return dr
+  }
+
+  createCreditObject () {
+    const dr = {}
+
+    ;[].forEach.call(this.credits, row => {
+      const type = row.querySelector('.new-item-card__credit-type').value
+      const amount = +row.querySelector('.new-item-card__credit-amount').value
+      dr[type] = amount
+    })
+
+    return dr
   }
 }
