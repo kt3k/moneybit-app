45ef6245404b1ca8a0b51aa7e422f7a2b11d95af
Yoshiya Hinosawa <stibium121@gmail.com>
2018-01-14 14:53:40 +0900

    feat: make new-item-card to component

--- src/pages/journal/new-item-card.js
+++ src/pages/journal/new-item-card.js
@@ -1,4 +1,4 @@
-const { capsid: { component, on, emits, wired }, Action } = require('~')
+const { capsid: { prep, component, on, emits, wired }, Action } = require('~')
 
 @component('js-new-item-card')
 export default class NewItemCard {
@@ -10,6 +10,76 @@ export default class NewItemCard {
   get debits () {}
   @wired.all('.new-item-card__credit')
   get credits () {}
+
+
+  __init__ () {
+    this.el.classList.add('card')
+    this.el.innerHTML = `
+      <div class="card-header">
+        <p class="card-header-title">
+          Date
+        </p>
+        <div class="card-header-icon">
+          <p class="control">
+            <input
+              class="input js-pickadate new-item-card__date"
+              value=""
+            />
+          </p>
+        </div>
+      </div>
+      <div class="card-content">
+        <div class="content">
+          <p class="t-text">app.description</p>
+          <p class="control"><input class="input new-item-card__desc" value=""></p>
+          <table>
+            <tr>
+              <th><t>domain.debit</t>
+              <th>
+            <tr class="new-item-card__debit">
+              <td>
+                <p class="control"><input class="input new-item-card__debit-type" value="普通預金"/>
+              <td>
+                <p class="control"><input class="input js-number-input new-item-card__debit-amount" />
+            <tr>
+              <td>
+                <button class="button is-primary is-outlined">
+                  <span class="icon">
+                    <i class="fa fa-plus"></i>
+                  </span>
+                </button>
+              <td>
+            <tr>
+              <th><t>domain.credit</t>
+              <th>
+            <tr class="new-item-card__credit">
+              <td>
+                <p class="control"><input class="input new-item-card__credit-type" value="元入金"/>
+              <td>
+                <p class="control"><input class="input js-number-input new-item-card__credit-amount" />
+            <tr>
+              <td>
+                <button class="button is-primary is-outlined">
+                  <span class="icon">
+                    <i class="fa fa-plus"></i>
+                  </span>
+                </button>
+              <td>
+          </table>
+        </div>
+      </div>
+      <div class="card-footer">
+        <p class="card-footer-item">
+          <a class="button is-danger t-text" href="#">ui.form.cancel</a>
+        </p>
+        <p class="card-footer-item">
+          <button class="button is-primary t-text new-item-save-button">ui.form.save</button>
+        </p>
+      </div>
+    `
+
+    prep(null, this.el)
+  }
 
   @on('click', { at: '.new-item-save-button' })
   @emits(Action.CREATE_TRADE)
