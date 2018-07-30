1e25ffb7dd709672a29805a7691791f936ab7089
Yoshiya Hinosawa <stibium121@gmail.com>
2018-07-30 15:23:44 +0900

    feat: add no-trade placeholder

---
+++ src/pages/journal/no-trade-placeholder.js
@@ -0,0 +1,19 @@
+const { component, on } = capsid
+
+@component('no-trade-placeholder')
+class NoTradePlaceholder {
+  @on(Action.MODEL_UPDATE)
+  update ({
+    detail: {
+      currentJournal: { trades }
+    }
+  }) {
+    if (trades.length > 0) {
+      this.el.style.display = 'none'
+    }
+
+    this.el.style.display = ''
+  }
+}
+
+module.exports = NoTradePlaceholder
