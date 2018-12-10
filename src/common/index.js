2653d5d6cf20e302a6b94e4b49864f8a226a93b1
Yoshiya Hinosawa <stibium121@gmail.com>
2018-12-10 23:38:31 +0900

    chore: update capsid/debug

--- src/common/index.js
+++ src/common/index.js
@@ -1,11 +1,11 @@
 require('@babel/polyfill')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('capsid/debug')
-}
 global.basepath = process.env.BASEPATH || ''
 global.t10 = require('t10')
 global.capsid = require('capsid')
+if (process.env.NODE_ENV !== 'production') {
+  global.capsid.install(require('capsid/debug'))
+}
 global.capsid.on.useHandler('change')
 global.capsid.on.useHandler('input')
 global.Action = require('../const/action-types.js')
