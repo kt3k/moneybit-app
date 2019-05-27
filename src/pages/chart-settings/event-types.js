804a33a66f2595d3d531fa5d44033fbbcb839372
Yoshiya Hinosawa <stibium121@gmail.com>
2019-05-27 14:29:10 +0900

    feat(chart-settings): add event types

---
+++ src/pages/chart-settings/event-types.js
@@ -0,0 +1,4 @@
+exports.OPEN_TOOLTIP = 'mb/chart-settings/OPEN_TOOLTIP'
+exports.OPEN_DELETE_MODAL = 'mb/chart-settings/OPEN_DELETE_MODAL'
+exports.OPEN_EDIT_MODAL = 'mb/chart-settings/OPEN_EDIT_MODAL'
+exports.OPEN_CREATE_MODAL = 'mb/chart-settings/OPEN_CREATE_MODAL'
